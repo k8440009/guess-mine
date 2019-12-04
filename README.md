3b6ca9967c69ffce56281d8dacfcdac0eb44368d
lucas0730 <lucas0730@naver.com>
2019-12-04 18:38:56 +0900

    Chat with SocketIO

--- README.md
+++ README.md
@@ -10,3 +10,14 @@ Realtime Drawing Game built with SocketIO, Gulp and Node
 - [x] ESLint
 - [] SocketIO
   http://localhost:4000/socket.io/socket.io.js (참고)
+
+## file
+
+- server.js : 서버, index.js : 클라이언트
+
+## SocketIO
+
+- 서버 껏다 켜도 다시 연결 가능 : Socket은 항상 서버 이벤트를 듣고 있기 때문
+  서버와 연결이 해제 되었다면 Socket은 계속 연결을 하려고 한다.
+- emit : 이벤트 발생, 연결된 socket에 이벤트 전송
+- broadcast : 방금 접속한 클라이언트를 제외한 모든 클라이언트들에게 메세지를 보낸다.
--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 import { join } from "path";
 import express from "express";
 import socketIO from "socket.io";
+import logger from "morgan";
 
 const PORT = 4000;
 const app = express();
 app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
+app.use(logger("dev"));
 app.use(express.static(join(__dirname, "static")));
 app.get("/", (req, res) => res.render("home"));
 
@@ -16,4 +18,22 @@ const handleListening = () =>
 
 // HTTP 서버위에 socketIO 서버를 올린다.
 const server = app.listen(PORT, handleListening);
-const io = socketIO(server);
+
+const io = socketIO.listen(server);
+
+let sockets = [];
+
+// 연결
+io.on("connection", socket => {
+  // 클라이언트가 전송한 메세지 다른 클라이언트들에게 전달
+  socket.on("newMessage", ({ message }) => {
+    socket.broadcast.emit("messageNotifi", {
+      message,
+      nickname: socket.nickname || "Anonymous"
+    });
+  });
+  // socket에 닉네임 저장
+  socket.on("setNickname", ({ nickname }) => {
+    socket.nickname = nickname;
+  });
+});
